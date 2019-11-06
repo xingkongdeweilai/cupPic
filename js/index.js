2533ead9e32b23e836bfb8930e4734d157a8f01e
刘力健 <liulijian@travelsky.com>
2019-11-06 10:37:29 +0800

    两个div的轮播图

--- js/index.js
+++ js/index.js
@@ -5,29 +5,74 @@ window.onload = function () {
     // $('.m-banner').mouseover(function () {
     //     bannerStopSlide();
     // });
-    bannerStartSlide();
+    setTimeout(bannerStartSlide, 2000);
 }
+
+let isOdd = true;
+let index = 2;
+let banner1 = $('.u-banner1');
+let banner2 = $('.u-banner2');
+let animateX1;
+let animateX2;
+let picSrcs = ["pic/banner/banner1.jpg"
+    , "pic/banner/banner2.jpg"
+    , "pic/banner/banner1.jpg"
+    , "pic/banner/banner2.jpg"
+    , "pic/banner/banner1.jpg"
+];
+let picWidth = '100%';
 
 /**
  * 轮播图开始滚动
  */
 function bannerStartSlide() {
     console.log('轮播图开始滚动');
-    let picSrcs = ["../pic/banner/banner1.jpg"
-        , "../pic/banner/banner2.jpg"
-        , "../pic/banner/banner3.jpg"
-        , "../pic/banner/banner4.jpg"];
-    let banner_width = $('.u-banner1').width();
-    let banner1 = $('.u-banner1');
-    let banner2 = $('.u-banner2');
-    $('.m-banner').animate({
-            right: '+=' + banner_width
+    if (isOdd) {
+        animateX1 = picWidth;
+        animateX2 = 0;
+    } else {
+        animateX1 = 0;
+        animateX2 = picWidth;
+    }
+
+    banner1.animate({
+            right: animateX1
         }
         , 2000
         , function () {
-            banner2.css({"background-image": "url('pic/banner/banner1.jpg')"});
-            banner1.css({"background-image": "url('pic/banner/banner2.jpg')"});
+            // afterSlide();
         });
+    banner2.animate({
+            right: animateX2
+        }
+        , 2000
+        , function () {
+            afterSlide();
+        });
+}
+
+function afterSlide() {
+    if (isOdd) {
+        banner1.css({
+            "background-image": "url('" + picSrcs[index] + "')",
+            "right": "-100%"
+        });
+        isOdd = false;
+    } else {
+        banner2.css({
+            "background-image": "url('" + picSrcs[index] + "')",
+            "right": "-100%"
+        });
+        isOdd = true;
+    }
+    if (index >= picSrcs.length - 1) {
+        index = 0;
+    } else {
+        index++;
+    }
+    setTimeout(function () {
+        bannerStartSlide();
+    }, 2000);
 }
 
 /**
